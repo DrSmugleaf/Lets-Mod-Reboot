d2ef541d74ab6172ead4984fc71728c6d4c8fb91
DrSmugleaf <javier.frayluis@gmail.com>
2015-02-25 16:21:13 +0100

    Configuration GUIs, logging, fixing configuration
    Episode 7

--- src/main/java/com/drsmugleaf/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/drsmugleaf/letsmodreboot/LetsModReboot.java
@@ -1,15 +1,17 @@
 package com.drsmugleaf.letsmodreboot;
 
-import com.drsmugleaf.letsmodreboot.configuration.ConfigurationHandler;
+import com.drsmugleaf.letsmodreboot.handler.ConfigurationHandler;
 import com.drsmugleaf.letsmodreboot.proxy.IProxy;
 import com.drsmugleaf.letsmodreboot.reference.Reference;
+import com.drsmugleaf.letsmodreboot.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -22,17 +24,19 @@ public class LetsModReboot
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialization Complete!");
     }
 }
